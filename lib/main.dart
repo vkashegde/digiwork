4364c3f6b089be215ee21dbfb6d2e4d9f3b4112f
vkashegde <vikas@kofluence.com>
2021-05-02 18:22:53 +0530

    search bar section added

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,119 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-            child: Container(
-          child: RecentCourseCard(
-            course: recentCourses[2],
+        body: Container(
+          color: kBackgroundColor,
+          child: SafeArea(
+            child: Column(
+              children: [
+                HomeScreenNavBar(),
+                RecentCourseCard(
+                  course: recentCourses[1],
+                ),
+              ],
+            ),
           ),
-        )),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SidebarButton(),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          CircleAvatar(
+            radius: 18,
+            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+      padding: EdgeInsets.only(left: 12, right: 33),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14),
+            boxShadow: [
+              BoxShadow(
+                  color: kShadowColor, offset: Offset(0, 12), blurRadius: 16),
+            ]),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            cursorColor: kPrimaryLabelColor,
+            decoration: InputDecoration(
+                icon: Icon(
+                  Icons.search,
+                  color: kPrimaryLabelColor,
+                  size: 20,
+                ),
+                border: InputBorder.none,
+                hintText: 'Search for courses',
+                hintStyle: kSearchPlaceholderStyle),
+            style: kSearchTextStyle,
+            onChanged: (newText) {
+              print(newText);
+            },
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class SidebarButton extends StatelessWidget {
+  const SidebarButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print("sidebar button pressed");
+      },
+      //it will only tap on content not outside.
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(maxWidth: 40, maxHeight: 40),
+
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14),
+            boxShadow: [
+              BoxShadow(
+                  color: kShadowColor, offset: Offset(0, 12), blurRadius: 16)
+            ]),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
       ),
     );
   }
