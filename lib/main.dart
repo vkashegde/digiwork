b879cea4723f8a93a0218e9945ed4a888c35103e
vkashegde <vikas@kofluence.com>
2021-05-03 22:39:54 +0530

    refactoring code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:digiwork/components/home_screen_navbar.dart';
+import 'package:digiwork/components/lists/explore_course_list.dart';
 import 'package:digiwork/components/sidebar_button.dart';
 import 'package:digiwork/constants.dart';
 import 'package:digiwork/model/course.dart';
@@ -62,81 +63,6 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 ExploreCourseList(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ExploreCourseList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: exploreCourses.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(left: index == 0 ? 20 : 0),
-            child: ExploreCourseCard(course: exploreCourses[index]),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class ExploreCourseCard extends StatelessWidget {
-  ExploreCourseCard({@required this.course});
-  final Course course;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: 20),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(41),
-        child: Container(
-          height: 120,
-          width: 280,
-          decoration: BoxDecoration(gradient: course.background),
-          child: Padding(
-            padding: EdgeInsets.only(left: 32),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        course.courseSubtitle,
-                        style: kCardSubtitleStyle,
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      Text(
-                        course.courseTitle,
-                        style: kCardTitleStyle,
-                      )
-                    ],
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Image.asset(
-                      'asset/illustrations/${course.illustration}',
-                      fit: BoxFit.cover,
-                      height: 100,
-                    ),
-                  ],
-                )
               ],
             ),
           ),
